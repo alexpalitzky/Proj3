1948c8ae0524ce1989fd4c8a0d2e33ca7f453680
Alex Palitzky <alexpa@manageware.co.il>
2015-06-07 17:15:12 +0300

    test

--- Ant-1/src/com/eureka/HelloWorld.java
+++ Ant-1/src/com/eureka/HelloWorld.java
@@ -4,6 +4,7 @@ package com.eureka;
 //another change
 //Third test
 //ksjdklfjaklsdjklf
+//asdfasdfqwer
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World");
