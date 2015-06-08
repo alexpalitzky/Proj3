8eef667f1dc2964e923b0dda0c3169a3abe8fd9b
Alex Palitzky <alexpa@manageware.co.il>
2015-06-08 09:11:40 +0300

    Alex1

--- Ant-1/src/com/eureka/HelloWorld.java
+++ Ant-1/src/com/eureka/HelloWorld.java
@@ -5,6 +5,7 @@ package com.eureka;
 //Third test
 //ksjdklfjaklsdjklf
 //asdfasdfqwer
+//Alex1
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World");
