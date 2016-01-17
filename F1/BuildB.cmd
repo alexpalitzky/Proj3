32a87c7c7017b01401e87a4763c7f59c356f9d03
Alex Palitzky <alexpa@manageware.co.il>
2016-01-17 09:10:35 +0200

    Change BuildB

--- F1/BuildB.cmd
+++ F1/BuildB.cmd
@@ -4,6 +4,8 @@ echo Build B
 
 sleep 10
 
-IF NOT %exit_code%==0 echo "##teamcity[setParameter name='env.RUN_TESTS_B' value='FALSE']" 
+IF  "%exit_code%"=="0" GOTO END
+echo ##teamcity[setParameter name='env.RUN_TESTS_B' value='FALSE']" 
 
+:END
 exit %exit_code%
