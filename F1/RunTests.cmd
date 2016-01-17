5af548c726aa0e7083ee95d7ee8dd615c3f7f677
Alex Palitzky <alexpa@manageware.co.il>
2016-01-17 10:28:02 +0200

    Change RunTests.cmd

--- F1/RunTests.cmd
+++ F1/RunTests.cmd
@@ -1,13 +1,15 @@
+echo off
+
 sleep 10
 
-echo %dep.SnapshotDependencies_BuildB.env.RUN_TESTS_B%
-IF "%dep.SnapshotDependencies_BuildB.env.RUN_TESTS_B%"=="FALSE" GOTO RUN_TESTS_C
+echo %dep.SnapshotDependenciesAlwaysRunningTests_B.env.RUN_TESTS_B%
+IF "%dep.SnapshotDependenciesAlwaysRunningTests_B.env.RUN_TESTS_B%"=="FALSE" GOTO RUN_TESTS_C
 FOR /L %%G IN (1,1,10) DO echo TEST-B-%%G 
 
 
 :RUN_TESTS_C
-echo %dep.SnapshotDependencies_BuildC.env.RUN_TESTS_C%
-IF "%dep.SnapshotDependencies_BuildC.env.RUN_TESTS_C%"=="FALSE" GOTO END
+echo %dep.SD_RunTests_BuildC.env.RUN_TESTS_C%%
+IF "%dep.SD_RunTests_BuildC.env.RUN_TESTS_C%"=="FALSE" GOTO END
 FOR /L %%G IN (1,1,10) DO echo TEST-C-%%G 
 
 
