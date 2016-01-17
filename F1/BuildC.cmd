9bad36d3302f0416af0b616bd7c46c4d62dfeef8
Alex Palitzky <alexpa@manageware.co.il>
2016-01-17 09:07:40 +0200

    Change BuildC

--- F1/BuildC.cmd
+++ F1/BuildC.cmd
@@ -4,10 +4,9 @@ echo Build C
 
 sleep 10
 
-IF NOT "%exit_code%"=="0"  GOTO SET_RUN_TESTS_C
-echo exit_code==0
 
-:SET_RUN_TESTS_C
-IF NOT "%exit_code%"=="0" echo ##teamcity[setParameter name='env.RUN_TESTS_C' value='FALSE']" 
+IF  "%exit_code%"=="0" GOTO END
+echo ##teamcity[setParameter name='env.RUN_TESTS_C' value='FALSE']" 
 
+:END
 exit %exit_code%
